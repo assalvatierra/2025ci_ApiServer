9d19065bfe500cac233aa2db2f67c016761b0cd3
Abel Salvatierra <assalvatierra@gmail.com>
2025-11-02 20:46:24 -0800

    Update ClassToTest_Tester.cs

--- TestProject1/ClassToTest_Tester.cs
+++ TestProject1/ClassToTest_Tester.cs
@@ -6,31 +6,67 @@ using System.Threading.Tasks;
 using ApiServer.Services;
 using System.Collections;
 using Moq;
+using Xunit.Abstractions;
 
 namespace TestProject1
 {
-    public class ClassToTest_Tester
+    [Collection("Test Collection")]
+    public class ClassToTest_Tester: IDisposable
     {
+        private readonly ITestOutputHelper output;
+        private testClass101 fixture;
+
+        public ClassToTest_Tester(ITestOutputHelper output, testClass101 fixture)
+        {
+            this.output = output;
+            this.fixture = fixture;
+        }
+
+        public void Dispose()
+        {
+            // Cleanup if needed
+        }
+
 
         [Theory]
         [MemberData(nameof(testClass101.getSimpleData), MemberType = typeof(testClass101))]
         public void addNumbers_simpleAdd(int num1, int num2, int expected)
         {
-            int addtionalNumber = 5;
+            int addtionalNumber = this.fixture.additionalNumber;
             var moqclass =  new Mock<IClassToTAdd>();
             moqclass.Setup(m => m.getAdditionalNumber()).Returns(addtionalNumber);
 
             ClassToTest ctt = new ClassToTest(moqclass.Object);
             int result = ctt.addNumberWithDependency(num1, num2);
             Assert.Equal(expected+addtionalNumber, result);
-        }   
 
+            this.output.WriteLine($"the result is {result}. Expecting: {expected}");
+        }
+
+        [Fact]
+        public void getAdditionalNumber()
+        {
+            Assert.NotEqual(this.fixture.additionalNumber, 0);
+        }
 
     }
 
 
+    [CollectionDefinition("Test Collection")]
+    public class TestCollection : ICollectionFixture<testClass101> { }
+
+
     public class testClass101
     {
+        public int additionalNumber = 0;
+
+        public testClass101()
+        {
+            Random rnd = new Random();
+            this.additionalNumber = rnd.Next(1, 10);
+
+        }
+
         public static IEnumerable<object[]> getSimpleData()
         {
             yield return new object[] { 1, 2, 3 };
@@ -40,6 +76,6 @@ namespace TestProject1
 
 
     }
-        
+
 
 }
